1bec0268c1410e501ac29e4879397a6beb5c5efa
Neaz-mq <neazmorshed666@gmail.com>
2025-08-25 10:44:24 +0600

    navbar update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,48 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const user = {};
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+     <header className="navbar">
+       <nav>
+         <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="SnapChat Logo"
+            width={32}
+            height={32}
+          />
+          <h1>Nexora</h1>
+        </Link>
+        {user && (
+          <figure>
+            <button>
+              <Image
+            src="/assets/images/dummy.jpg"
+            alt="User"
+            width={36}
+            height={36}
+             className="rounded-full aspect-square"
+          />
+            </button>  
+
+            <button       
+              className="cursor-pointer"
+            >
+              <Image
+                src="/assets/icons/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+                className="rotate-180"
+              />
+            </button>     
+          </figure>
+          )}
+       </nav>
+        </header>
   )
 }
 
