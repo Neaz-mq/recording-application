5d90642798d3b6792cde95a92f1b2030a1d20de5
Neaz-mq <neazmorshed666@gmail.com>
2025-08-25 11:39:47 +0600

    videocard working

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,19 @@
+import VideoCard from '@/components/VideoCard'
 import React from 'react'
 
 const page = () => {
   return (
      <main className="wrapper page">
       <h1 className='text-6xl text-amber-500'>Welcome</h1>
+      <VideoCard 
+    
+              id= "1"
+              title="hello"
+              thumbnail="/assets/images/video1.png"
+              createdAt="10"
+             
+      
+      />
      </main>
   )
 }
--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,8 +1,53 @@
-import React from 'react'
+"use client";
 
-const VideoCard = () => {
+import Link from 'next/link'
+import React from 'react'
+import Image from "next/image";
+
+const VideoCard = ({id, title, thumbnail, createdAt}: VideoCardProps) => {
   return (
-    <div>VideoCard</div>
+    <Link href={`/video/${id}`} className="video-card">
+ <Image
+        src={thumbnail}
+        width={290}
+        height={160}
+        alt="thumbnail"
+        className="thumbnail"
+      />
+       <article>
+        <div>
+          <figure>
+            <Image
+              src=''
+              width={34}
+              height={34}
+              alt="avatar"
+              className="rounded-full aspect-square"
+            />
+            <figcaption>
+              <h3>gfg</h3>
+              <p>fd</p>
+            </figcaption>
+          </figure>
+          <aside>
+            <Image
+              src="/assets/icons/eye.svg"
+              alt="views"
+              width={16}
+              height={16}
+            />
+            <span>20</span>
+          </aside>
+        </div>
+        <h2>
+          {title} -{" "}
+         
+        </h2>
+      </article>
+        <button  className="copy-btn">
+       
+      </button>
+        </Link>
   )
 }
 
