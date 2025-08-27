321549ddd7b8e33ba0301a886b00aea4fede5394
Neaz-mq <neazmorshed666@gmail.com>
2025-08-27 15:12:10 +0600

    utils.ts working

--- constants/index.ts
+++ constants/index.ts
@@ -3,10 +3,10 @@ export const MAX_THUMBNAIL_SIZE = 10 * 1024 * 1024;
 
 export const BUNNY = {
   STREAM_BASE_URL: "https://video.bunnycdn.com/library",
-  STORAGE_BASE_URL: "https://sg.storage.bunnycdn.com/nexora",
-  CDN_URL: "https://nexora.b-cdn.net",
+  STORAGE_BASE_URL: "https://storage.bunnycdn.com/neaz-nexora",
+  CDN_URL: "https://neaz-nexora.b-cdn.net",
   EMBED_URL: "https://iframe.mediadelivery.net/embed",
-  TRANSCRIPT_URL: "https://vz-47a08e64-84d.b-cdn.net",
+  TRANSCRIPT_URL: "https://vz-a672dea5-77b.b-cdn.net",
 };
 
 export const emojis = ["😂", "😍", "👍"];
