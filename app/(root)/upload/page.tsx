0c70023beb98c006e095393c2f617d942734e42a
Neaz-mq <neazmorshed666@gmail.com>
2025-08-27 10:55:43 +0600

    formfield working

---
+++ app/(root)/upload/page.tsx
@@ -0,0 +1,14 @@
+import FileInput from '@/components/FileInput'
+import FormField from '@/components/FormField'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <FormField />
+        <FileInput />
+    </div>
+  )
+}
+
+export default page
